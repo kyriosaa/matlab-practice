2c45c58a7ef9f4a2685be4bee463b95b5855306e
kyriosaa <kyriosaaph@gmail.com>
2025-10-25 16:53:21 +0800

    matlab book 1.14

--- programming_matlab/ch1_matlab_environment/ch1_14_problems.m
+++ programming_matlab/ch1_matlab_environment/ch1_14_problems.m
@@ -6,26 +6,27 @@
     % sin(x)cos(x)          for -10 <= x <= 10
     % cos(x)sin(x)exp(x/10) for  -1 <= x <=  1
 
-    % x = linspace(-10, 10, 500);
-    % y = sin(x);
-    % plot(x, y)
-    % title('1-1 sin(x)')
-    % pause;
-    % 
-    % x = linspace(-10, 10, 500);
-    % y1 = sin(x);
-    % y2 = cos(x);
-    % plot(x, y1, x, y2)
-    % title('1-1 sin(x)cos(x)')
-    % pause;
-    % 
-    % x = linspace(-1, 1, 100);
-    % y1 = cos(x);
-    % y2 = sin(x);
-    % y3 = exp(x/10);
-    % plot(x, y1, x, y2, x, y3)
-    % title('1-1 cos(x)sin(x)exp(x/10)')
-    % pause;
+    x = linspace(-10, 10, 500);
+    y = sin(x);
+    plot(x, y)
+    title('1-1 sin(x)')
+    pause;
+
+    x = linspace(-10, 10, 500);
+    y1 = sin(x);
+    y2 = cos(x);
+    plot(x, y1, x, y2)
+    title('1-1 sin(x)cos(x)')
+    pause;
+
+    x = linspace(-1, 1, 100);
+    y1 = cos(x);
+    y2 = sin(x);
+    y3 = exp(x/10);
+    plot(x, y1, x, y2, x, y3)
+    title('1-1 cos(x)sin(x)exp(x/10)')
+    pause;
+    
 % ------------------------------------------------------------------------
 % 1-2: Write a script that does the following:
     % 1. Creates a random 5x5 matrix, displays that random matrix,
@@ -39,11 +40,13 @@
     %    first character was, what the second character was, and what the
     %    third character was.
 
+    % 1
     rand_matrix = rand(5, 5)
     inv_rand_matrix = inv(rand_matrix)
     final_matrix = rand_matrix*inv_rand_matrix
     pause;
 
+    % 2
     p1 = input('Input phrase 1: ', 's');
     p2 = input('Input phrase 2: ', 's');
     p3 = input('Input phrase 3: ', 's');
@@ -52,7 +55,22 @@
     fprintf('\n');
     pause;
 
-    
+    % 3
+    original = input('What''s a phrase that contains all letters of the English alphabet?: ', 's');
+    replaced = strrep(original, 'the', 'hey');
+    fprintf(replaced);
+    fprintf('\n');
+    pause;
+
+    % 4
+    usr_string = input('Input any text string longer than 3 characters: ', 's');
+    first_char = usr_string(1);
+    second_char = usr_string(2);
+    third_char = usr_string(3);
+    fprintf('First character: %s\n', first_char);
+    fprintf('Second character: %s\n', second_char);
+    fprintf('Third character: %s\n', third_char);
+  
 % ------------------------------------------------------------------------
 % 1-3: Create a script that does the following:
     % 1. Displays your name and major.
@@ -69,6 +87,39 @@
     % 9. Label the x and y axes using the MATLAB xlabel() and ylabel()
     %    functions.
 
+    % 1
+    fprintf('Name: kyriosaa\n');
+    fprintf('Major: Electrical Engineering and Computer Science\n');
+
+    % 2
+    pause(5);
+
+    % 3
+    x = input('Input a number between 50 and 100: ', 's');
+
+    % 4
+    x = str2double(x);
+    sqrt_x = sqrt(x);
+    format long;
+    fprintf('%f', sqrt_x);
+
+    % 5
+    pause;
+
+    % 6
+    time = linspace(0, 0.1, 100);
+
+    % 7
+    y = sin(2 * pi * x * time)
+
+    % 8 
+    figure;
+    plot(time, y);
+
+    % 9
+    title('y = sin(2πxt)');
+    xlabel('Time (s)');
+    ylabel('y');
 
 % ------------------------------------------------------------------------
 % 1-4: Find the solution (A, B, C) to the system of equations:
@@ -76,4 +127,15 @@
     %    0 = -175A +  800B - 250C
     %    5 =         -300B + 600C
 
-    
+    A = [ 400, -175,    0;
+         -175,  800, -250;
+            0, -300,  600];
+    B = [10, 0, 5];
+
+    answer = A/B;
+
+    ans_A = answer(1);
+    ans_B = answer(2);
+    ans_C = answer(3);
+
+    fprintf('A = %f, B = %f, C = %f', ans_A, ans_B, ans_C);
---
+++ programming_matlab/ch2_control_flow/ch2_01_if_end.m
@@ -0,0 +1,3 @@
+% 2.1 IF-END
+
+% simple number guessing game
