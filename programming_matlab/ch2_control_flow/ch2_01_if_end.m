733e8e9130b3d8336ab0084902ea8c946c527ea7
kyriosaa <kyriosaaph@gmail.com>
2025-10-26 17:36:52 +0800

    rename ecad folder

--- programming_matlab/ch2_control_flow/ch2_01_if_end.m
+++ programming_matlab/ch2_control_flow/ch2_01_if_end.m
@@ -1,3 +1,17 @@
 % 2.1 IF-END
 
 % simple number guessing game
+
+number = rand(1);
+number = 10*number;
+number = ceil(number);
+
+guess = input('Guess the number between 1 and 10: ');
+
+if guess == number
+    fprintf('Correct! You win!\n');
+end
+
+if guess ~= number
+    fprintf('Wrong... The number I was thinking of was %d. Try again.\n', number);
+end
